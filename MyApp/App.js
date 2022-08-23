27d4f27105e0536a2ca2a075c667003962d9599e
Anderson Marlon <anderson18.marlon@gmail.com>
2022-08-23 17:06:16 -0300

    Initial commit

---
+++ MyApp/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>I got black</Text>
+      <Text>I got white</Text>
+      <Text>What you want</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
