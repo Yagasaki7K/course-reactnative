99e068ededc8d206a7f88ff99273bad2be55202d
Anderson Marlon <anderson18.marlon@gmail.com>
2022-08-23 18:48:58 -0300

    Added: Style

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,22 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>I got black</Text>
-      <Text>I got white</Text>
-      <Text>What you want</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let FindYourPet = 'https://findyourpet.vercel.app/assets/logo-white.540e2d43.png'
+
+    return (
+        <View style={styles.container}>
+
+            <Image 
+            source={{uri: FindYourPet}}
+            style={{height: 100, width: 100}}/>
+
+
+            <Text style={{marginTop: 50}}>I got black</Text>
+            <Text style={styles.text}>I got white</Text>
+            <Text>What you want</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#703edb',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    text: {
+        color: '#fff'
+    }
 });
